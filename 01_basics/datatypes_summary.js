e3ac57f643807cb447b126eddf6921df84924ee7
Akash Babu <akashkdug@gmail.com>
2025-12-15 01:50:16 +0530

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+/* Primitive  (primitive datatypes are call by value)
+
+ 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ */
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 5847597438943n
+
+
+/*
+        Reference ( Non Primitive)
+        Arry, Objects, Functions
+
+*/
+const heros = ["shaktman", "naagraj","doga"]
+let myObj ={
+    name: "akash",
+    age: 22
+} 
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof myFunction)
+console.log(typeof heros)
+console.log(typeof myObj)
