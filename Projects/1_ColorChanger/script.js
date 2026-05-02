ffa906105848042f5b09f690efce865b3013e1d8
Akash <akashkdug@gmail.com>
2026-05-02 17:39:35 +0530

    project 1 Colour switcher is dont

--- Projects/1_ColorChanger/script.js
+++ Projects/1_ColorChanger/script.js
@@ -1,7 +1,26 @@
 const buttons = document.querySelectorAll('.button')
 // console.log(buttons)
-const body = document.querySelector('body')
+const body = document.querySelector("body")
 
 buttons.forEach((button)=> {
-    console.log(button)
-})
+    console.log(button);
+    button.addEventListener('click', function (e){
+      console.log(e);
+      if(e.target.id === 'grey'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'white'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'yellow'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'purple'){
+        body.style.backgroundColor = e.target.id;
+      }
+
+    });
+});
